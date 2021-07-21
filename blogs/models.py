ba98eea0f337f06cc1892e32c72e920f917573fa
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-07-21 19:51:58 +0530

    form handling

--- blogs/models.py
+++ blogs/models.py
@@ -4,18 +4,17 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.user_name
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='blogs')
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET("Anonymous"), related_name='blogs')
     date = models.DateField(auto_now=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Blog
-from .forms import  BlogForm
+from django.shortcuts import render, redirect
+from .models import Blog, Author
+from .forms import BlogForm
+from datetime import date
 
 
 # Create your views here.
@@ -24,7 +25,24 @@ def detail_blogs(request, pk):
 
 
 def write_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+
+        if form.is_valid():
+            new_author = Author(user_name=request.user.username,
+                                email=request.user.email)
+            new_author.save()
+            print("Author = ", request.user.email)
+            new_blog = Blog(author=new_author, title=request.POST['title'],
+                            excerpt=request.POST['excerpt'], content=request.POST['content'], date=date.today())
+            new_blog.save()
+            return redirect('all-blogs')
+        else:
+            return render(request, 'blogs/create.html', {
+                'form': form
+            })
+
     form = BlogForm()
     return render(request, 'blogs/create.html', {
-        'form' : form
+        'form': form
     })
